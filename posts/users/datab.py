589bd11e5c5e219e750cc95d914da1db525c95a4
Mabelt83 <137824865+Mabelt83@users.noreply.github.com>
2023-10-08 22:49:31 -0400

    hw

---
+++ posts/users/datab.py
@@ -0,0 +1,25 @@
+users = {
+  '1':{
+    'username':'seanc',
+    'email':'seanc@ct.com'
+  },
+  '2':{
+    'username':'dylans',
+    'email':'dylans@ct.com'
+  }
+}
+
+posts = {
+  '1':{
+    'body':'Welcome to Flask Week',
+    'user_id': '2'
+  },
+  '3':{
+    'body':'SERVERS',
+    'user_id': '2'
+  },
+  '2':{
+    'body':"My favorite Week",
+    'user_id': '1'
+  }
+}
